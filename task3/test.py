ca31f4af0ee4d179a8b28dff3f32c90cf2696fad
Mykhailo Holub <mishataube2005@gmail.com>
2025-03-01 23:46:48 +0200

    Update test.py

--- task3/test.py
+++ task3/test.py
@@ -4,9 +4,44 @@ from ByteCounter import ByteCounter
 from HuffmanTree import HuffmanTree
 from time import time, sleep
 from HuffmanFileCoder import huffman_encode, huffman_decode
+import os
+from PythonTableConsole import PythonTableConsole
+import filecmp
 
-source = "files/ДЗ6.pdf"
+raw_folder = "files/raw"
+encoded_folder = "files/encoded"
+decoded_folder = "files/decoded"
+log_path = "files/test_log.txt"
 
-huffman_encode(source)
-sleep(1)
-huffman_decode(source + ".huff", "files/ДЗ6_decoded.pdf")
+raw_files = os.listdir(raw_folder)
+
+table = ["name", "raw length, KB", "encoded length, KB", "compression, %", "time to encode, s", "time to decode, s", "all correct"]
+
+for file in raw_files:
+    print(file)
+    raw_path = raw_folder + "/" + file
+    encoded_path = encoded_folder + "/" + file + ".huff"
+    decoded_path = decoded_folder + "/" + file
+
+    time_to_encode = -time()
+    huffman_encode(raw_path, encoded_path)
+    time_to_encode += time()
+
+    time_to_decode = -time()
+    huffman_decode(encoded_path, decoded_path)
+    time_to_decode += time()
+
+    table.append([
+        file,
+        os.path.getsize(raw_path)//1024,
+        os.path.getsize(encoded_path)//1024,
+        round(os.path.getsize(encoded_path)*100 / os.path.getsize(encoded_path),1),
+        time_to_encode,
+        time_to_decode,
+        filecmp.cmp(raw_path, encoded_path)
+    ])
+    sleep(0.5)
+
+table = PythonTableConsole(table)
+table.transpose()
+print(table)
